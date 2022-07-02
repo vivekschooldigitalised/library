f29a6bed375d28e1699accf04a45e2ad5fc43505
SCHOOL DIGITALISATION <103983900+vivekschooldigitalised@users.noreply.github.com>
2022-07-02 08:51:08 +0530

    update

---
+++ app/Http/Controllers/CountIndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class CountIndexController extends Controller
+{
+
+        public function countmethod1(Request $request)
+        {
+            
+        } 
+     
+}
